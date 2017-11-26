64d768997090ea9202f94ba9093930d64245271b
kyleplo <31634240+kyleplo@users.noreply.github.com>
2017-11-26 14:05:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ var d = new Date();
 document.getElementById("date").innerHTML = months[d.getMonth()] + " " + d.getDate();
 if(d.getMonth() < 11 && d.getMonth() > 5){document.getElementById("msg1").removeAttribute("hidden")};
 if(d.getMonth() === 11 && d.getDate() === 25){document.getElementById("msg2").removeAttribute("hidden")};
-if(d.getMonth() === 11 && d.getDate() > 24){document.getElementById("msg3").removeAttribute("hidden");loadDays(23)};
+if(d.getMonth() === 11 && d.getDate() > 24){document.getElementById("msg3").removeAttribute("hidden");loadDays(24)};
 if(d.getMonth() === 11 && d.getDate() < 25){loadDays(d.getDate() - 1)};
-if(d.getMonth() < 6){document.getElementById("msg3").removeAttribute("hidden");loadDays(23)};
+if(d.getMonth() < 6){document.getElementById("msg3").removeAttribute("hidden");loadDays(24)};
 function loadDays(days){
 var main = document.querySelector("main");
 for(var i = 0;i < days;i++){main.innerHTML += '<a href="#' + i + '" class="day">' + (i + 1) + '</a> '};
@@ -21,7 +21,7 @@ document.getElementById("close").addEventListener("click",function (){document.g
 window.addEventListener("hashchange", showDay);
 window.addEventListener("load", showDay);
 function showDay(){
-var day = location.hash.slice(1);
+if(location.hash.length > 0){var day = location.hash.slice(1);
 document.getElementById("popup").removeAttribute("hidden");
 document.getElementById("title").innerHTML = "December " + (day + 1);
 document.getElementById("text").innerHTML = data.data[day].p;
@@ -29,4 +29,4 @@ document.getElementById("credit").innerHTML = data.data[day].s;
 document.getElementById("link").setAttribute("href",data.data[day].l);
 document.getElementById("share").setAttribute("href","https://kyleplo.github.io/advent-calender#" + day);
 console.log("Popup loaded.");
-}
+}}
